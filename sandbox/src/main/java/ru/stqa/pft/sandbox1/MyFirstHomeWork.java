bae7a59a2a4feeac21bb26ed1d4d6ba86e732a4f
Ульяна Головачева <ulitaaaa@mail.ru>
2021-10-03 00:20:15 +0300

    2.3 запуск класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstHomeWork.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstHomeWork.java
@@ -4,12 +4,8 @@ public class MyFirstHomeWork {
 
   public static void main (String[] args){
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 4;
-    p1.y = 6;
-    p2.x = -1;
-    p2.y = 0;
+    Point p1 = new Point(4,6);
+    Point p2 = new Point(-1,0);
     System.out.println("Расстояние между точками " + "А(" + p1.x + ";" + p1.y + ")" + " и " + "В(" + p2.x + ";" + p2.y + ")" + " равно " + distance(p1,p2));
   }
   public static double distance(Point p1,Point p2){
