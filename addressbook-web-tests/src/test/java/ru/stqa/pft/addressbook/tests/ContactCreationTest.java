e9375fa3b06971fe52113dafcf7ccd6d9060535c
Ульяна Головачева <ulitaaaa@mail.ru>
2021-10-12 15:24:55 +0300

    Create ContactCreationTest.java

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTest extends TestBase {
+
+  @Test
+  public void testContactCreation() throws Exception {
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2", "Rostov-na-Donu,\nTheatralniy, 60", "8999999991", "ulita_rnd@bk.ru", "9", "October"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().goToHomeContactPage();
+    app.getContactHelper().returnToContactPage();
+  }
+}
