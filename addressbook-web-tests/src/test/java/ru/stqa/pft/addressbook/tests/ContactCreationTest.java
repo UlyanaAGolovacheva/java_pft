7ff8965c1166c21e3a9aaa905941860cf890ae2d
Ульяна Головачева <ulitaaaa@mail.ru>
2021-10-12 13:07:06 +0300

    Update ContactCreationTest.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -8,7 +8,7 @@ public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2", "Rostov-na-Donu,\nTheatralniy, 60", "89081903221", "ulita_rnd@bk.ru", "9", "October"));
+    app.getContactHelper().fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2", "Rostov-na-Donu,\nTheatralniy, 60", "8999999991", "ulita_rnd@bk.ru", "9", "October"));
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().goToHomeContactPage();
     app.getContactHelper().returnToContactPage();
